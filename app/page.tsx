34f8327adfeba29a9d1765b1a61d51c945949d02
gulfcoastorganics-ai <gulfcoastorganics@gmail.com>
2026-05-21 11:00:43 -0500

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import HeroSection from '@/components/organisms/HeroSection';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black">
+      <HeroSection />
+    </main>
+  );
+}
